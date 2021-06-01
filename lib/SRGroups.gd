829ce52e3408ff526cbc3dbe84a5a901bf4bd63f
SamSGKing <sam.king@newcastle.edu.au>
2021-06-01 16:48:36 +1000

    Implemented AllSRGroups Function

--- lib/SRGroups.gd
+++ lib/SRGroups.gd
@@ -16,31 +16,49 @@
 
 #! @Description
 #!   Insert documentation for your function here
-DeclareGlobalFunction( "SRGroup" );
-DeclareGlobalFunction( "AllSRGroups" );
-DeclareGlobalFunction( "CheckSRProjections" );
-DeclareGlobalFunction( "StringVariables" );
-DeclareGlobalFunction( "UnbindVariables" );
+
+
+# a new "category" for the groups acting on regular rooted trees that we study, based on the category of permutation groups
+DeclareCategory("IsRegularRootedTreeGroup", IsPermGroup);
+
+# a creator function asking for depth, degree, and the permutation group
+DeclareOperation("RegularRootedTreeGroup", [IsInt, IsInt, IsPermGroup]);
+
+# degree and depth are attributes of groups of this type
+DeclareAttribute("RegularRootedTreeGroupDegree", IsRegularRootedTreeGroup);
+DeclareAttribute("RegularRootedTreeGroupDepth", IsRegularRootedTreeGroup);
+
+# being self-replicating and having sufficient rigid automorphisms are properties (i.e. boolean attributes)
+DeclareProperty("IsSelfReplicating", IsRegularRootedTreeGroup);
+DeclareProperty("HasSufficientRigidAutomorphisms", IsRegularRootedTreeGroup);
+
+# parent group (projection), maximal extension and representative with sufficient rigid automorphisms also become attributes
+DeclareAttribute("ParentGroup", IsRegularRootedTreeGroup);
+DeclareAttribute("MaximalExtension", IsRegularRootedTreeGroup);
+DeclareAttribute("RepresentativeWithSufficientRigidAutomorphisms", IsRegularRootedTreeGroup);
+
+DeclareGlobalFunction("AllSRGroups");
+DeclareGlobalFunction( "SRGroupsInfo" );
+DeclareGlobalFunction( "AllSRGroupsInfo" );
+# DeclareGlobalFunction( "CheckSRProjections" );
+# DeclareGlobalFunction( "StringVariables" );
+# DeclareGlobalFunction( "UnbindVariables" );
 DeclareGlobalFunction( "SRDegrees" );
 DeclareGlobalFunction( "SRLevels" );
 
 DeclareGlobalFunction( "AutT" );
-DeclareGlobalFunction( "IsSelfReplicating" );
 DeclareGlobalFunction( "BelowAction" );
-DeclareGlobalFunction( "MaximalExtension" );
-DeclareGlobalFunction( "HasSufficientRigidAutomorphisms" );
-DeclareGlobalFunction( "RepresentativeWithSufficientRigidAutomorphisms" );
-DeclareGlobalFunction( "ConjugacyClassRepsMaxSelfReplicatingSubgroups" );
-DeclareGlobalFunction( "ConjugacyClassRepsMaxSelfReplicatingSubgroupsWithProjection" );
-DeclareGlobalFunction( "ConjugacyClassRepsSelfReplicatingSubgroups" );
-DeclareGlobalFunction( "ConjugacyClassRepsSelfReplicatingSubgroupsWithProjection" );
-DeclareGlobalFunction( "FormatSRFile" );
-DeclareGlobalFunction( "SRGroupFile" );
-DeclareGlobalFunction( "HasseDiagram" );
-DeclareGlobalFunction( "ExtensionsMapping" );
-DeclareGlobalFunction( "PermutationMapping" );
-DeclareGlobalFunction( "ExtendSRGroup" );
-DeclareGlobalFunction( "CombineSRFiles" );
-DeclareGlobalFunction( "ReorderSRFiles" );
-DeclareGlobalFunction( "NumberExtensionsUnformatted" );
+# DeclareGlobalFunction( "ConjugacyClassRepsMaxSelfReplicatingSubgroups" );
+# DeclareGlobalFunction( "ConjugacyClassRepsMaxSelfReplicatingSubgroupsWithProjection" );
+# DeclareGlobalFunction( "ConjugacyClassRepsSelfReplicatingSubgroups" );
+# DeclareGlobalFunction( "ConjugacyClassRepsSelfReplicatingSubgroupsWithProjection" );
+# DeclareGlobalFunction( "FormatSRFile" );
+# DeclareGlobalFunction( "SRGroupFile" );
+# DeclareGlobalFunction( "HasseDiagram" );
+# DeclareGlobalFunction( "ExtensionsMapping" );
+# DeclareGlobalFunction( "PermutationMapping" );
+# DeclareGlobalFunction( "ExtendSRGroup" );
+# DeclareGlobalFunction( "CombineSRFiles" );
+# DeclareGlobalFunction( "ReorderSRFiles" );
+# DeclareGlobalFunction( "NumberExtensionsUnformatted" );
 DeclareGlobalFunction( "Level" );
