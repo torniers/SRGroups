54a2adff089197c563a284d412d5ef701be60e44
SamSGKing <sam.king@newcastle.edu.au>
2021-06-08 09:46:37 +1000

    Update SRGroups.gd

--- lib/SRGroups.gd
+++ lib/SRGroups.gd
@@ -1,21 +1,40 @@
-#
-# SRGroups: Self-replicating groups of regular rooted trees.
-#
+#! @Title SRGroups: Self-replicating groups of regular rooted trees.
+
+##################################################################################################################
+#! @Abstract
+##################################################################################################################
+
+#! To do.
+
+##################################################################################################################
+#! @Copyright
+##################################################################################################################
+
+#! <Package>???</Package> is free software; you can redistribute it and/or modify it under the terms of the <URL Text="GNU General Public License">http://www.fsf.org/licenses/gpl.html</URL> as published by the Free Software     Foundation; either version 3 of the License, or (at your option) any later version.
+
+##################################################################################################################
+#! @Acknowledgements
+##################################################################################################################
+
+#! DE210100180, FL170100032.
+
+####################################################################################################################
+####################################################################################################################
 #! @Chapter Introduction
-#!
-#! SRGroups is a package which does some
-#! interesting and cool things
-#!
-#! @Chapter Functionality
-#!
-#!
-#! @Section Example Methods
-#!
-#! This section will describe the example
-#! methods of SRGroups
+####################################################################################################################
+####################################################################################################################
+
+#! SRGroups is a package which does some interesting and cool things. To be continued...
+
+####################################################################################################################
+#! @Chapter Framework
+####################################################################################################################
 
 # a new "category" for the groups acting on regular rooted trees that we study, based on the category of permutation groups
 DeclareCategory("IsRegularRootedTreeGroup", IsPermGroup);
+DeclareGlobalFunction("AllSRGroups");
+#! @Description
+#!   Insert documentation for your function here
 
 # a creator function asking for depth, degree, and the permutation group
 DeclareOperation("RegularRootedTreeGroup", [IsInt, IsInt, IsPermGroup]);
@@ -32,6 +51,10 @@ DeclareProperty("HasSufficientRigidAutomorphisms", IsRegularRootedTreeGroup);
 DeclareAttribute("ParentGroup", IsRegularRootedTreeGroup);
 DeclareAttribute("MaximalExtension", IsRegularRootedTreeGroup);
 DeclareAttribute("RepresentativeWithSufficientRigidAutomorphisms", IsRegularRootedTreeGroup);
+
+####################################################################################################################
+#! @Chapter Methods
+####################################################################################################################
 
 # Library Functions
 DeclareGlobalFunction("AllSRGroups");
@@ -50,7 +73,22 @@ DeclareGlobalFunction( "SRDegrees" );
 DeclareGlobalFunction( "SRLevels" );
 
 # Package Functions
+#! @Description
+#! The arguments of this method are a degree <A>k</A> $\in\mathbb{N}_{\ge 2}$ and a depth <A>n</A> $\in\mathbb{N}$.
+#!
+#! @Returns
+#! the regular rooted tree group $\mathrm{Aut}(T_{k,n})$ as a permutation group of the $k^{n}$ leaves of $T_{k,n}$.
+#!
+#! @Arguments k,n
 DeclareGlobalFunction( "AutT" );
+#!
+#! @BeginExampleSession
+#! gap> G:=AutT(2,2);
+#! Group([ (1,2), (3,4), (1,3)(2,4) ])
+#! gap> Size(G);
+#! 8
+#! @EndExampleSession
+
 DeclareGlobalFunction( "BelowAction" );
 # DeclareGlobalFunction( "ConjugacyClassRepsMaxSelfReplicatingSubgroups" );
 # DeclareGlobalFunction( "ConjugacyClassRepsMaxSelfReplicatingSubgroupsWithProjection" );
