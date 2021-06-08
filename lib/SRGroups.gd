83d0478ecf4649edfb991c6772d2dd067bec1a6c
SamSGK <sam.king@newcastle.edu.au>
2021-06-08 16:00:29 +1000

    Updating Documentation

--- lib/SRGroups.gd
+++ lib/SRGroups.gd
@@ -10,7 +10,7 @@
 #! @Copyright
 ##################################################################################################################
 
-#! <Package>???</Package> is free software; you can redistribute it and/or modify it under the terms of the <URL Text="GNU General Public License">http://www.fsf.org/licenses/gpl.html</URL> as published by the Free Software     Foundation; either version 3 of the License, or (at your option) any later version.
+#! <Package>SRGroups</Package> is free software; you can redistribute it and/or modify it under the terms of the <URL Text="GNU General Public License">http://www.fsf.org/licenses/gpl.html</URL> as published by the Free Software     Foundation; either version 3 of the License, or (at your option) any later version.
 
 ##################################################################################################################
 #! @Acknowledgements
@@ -32,11 +32,14 @@
 
 #! @Section Methods
 # a new "category" for the groups acting on regular rooted trees that we study, based on the category of permutation groups
-DeclareCategory("IsRegularRootedTreeGroup", IsPermGroup);
 #! @Description
-#!   Insert documentation for your function here
+#! Checks whether the input group is a regular-rooted tree group.
+DeclareCategory("IsRegularRootedTreeGroup", IsPermGroup);
+
 
 # a creator function asking for depth, degree, and the permutation group
+#! @Description
+#! Creates a regular-rooted tree group with attributes <A>RegularRootedTreeGroupDegree</A> and <A>RegularRootedTreeGroupDepth</A>.
 DeclareOperation("RegularRootedTreeGroup", [IsInt, IsInt, IsPermGroup]);
 
 # degree and depth are attributes of groups of this type
@@ -57,20 +60,50 @@ DeclareAttribute("RepresentativeWithSufficientRigidAutomorphisms", IsRegularRoot
 ####################################################################################################################
 
 # Library Functions
+#! @Description
+#! Main library search function. Has several possible input arguments such as <A>Degree</A>, <A>Level</A> (or <A>Depth</A>), <A>Number</A>, <A>Projection</A>, <A>Subgroup</A>, <A>Size</A>, <A>NumberOfGenerators</A>, and <A>IsAbelian</A>.
 DeclareGlobalFunction("AllSRGroups");
+#! @BeginExampleSession
+#! gap> AllSRGroups(Degree, 2, Level, 4, IsAbelian, true);
+#! [ SRGroup(2,4,2), SRGroup(2,4,9), SRGroup(2,4,12), SRGroup(2,4,14) ]
+#! gap> Size(last[1]);
+#! 16
+#! gap> AllSRGroups(Degree,2,Level,4,NumberOfGenerators,4);
+#! [ SRGroup(2,4,11), SRGroup(2,4,12), SRGroup(2,4,16), SRGroup(2,4,20), SRGroup(2,4,23), SRGroup(2,4,24),
+#!  SRGroup(2,4,25), SRGroup(2,4,26), SRGroup(2,4,40), SRGroup(2,4,43), SRGroup(2,4,46), SRGroup(2,4,47),
+#!  SRGroup(2,4,50), SRGroup(2,4,66), SRGroup(2,4,70), SRGroup(2,4,71), SRGroup(2,4,72), SRGroup(2,4,73),
+#!  SRGroup(2,4,74), SRGroup(2,4,75), SRGroup(2,4,76), SRGroup(2,4,84), SRGroup(2,4,90), SRGroup(2,4,91),
+#!  SRGroup(2,4,93), SRGroup(2,4,95), SRGroup(2,4,97), SRGroup(2,4,102), SRGroup(2,4,108) ]
+#! @EndExampleSession
 #! @Description
 #!   Insert documentation for your function here
 DeclareGlobalFunction( "SRGroupsInfo" );
 #! @Description
-#!   Insert documentation for your function here
+#! Works the same as the main library search function <A>AllSRGroups</A>, except returns useful information about the group(s) in list form: [<A>Generators</A>, <A>Name</A>, <A>Parent Name</A>, <A>Children Names</A>].
 DeclareGlobalFunction( "AllSRGroupsInfo" );
-#! @Description
-#!   Insert documentation for your function here
+#! @BeginExampleSession
+#! gap> AllSRGroupsInfo(Degree, 2, Level, 3, IsAbelian, true);
+#! [ [ [ (1,5,4,8,2,6,3,7), (1,4,2,3)(5,8,6,7), (1,2)(3,4)(5,6)(7,8) ], "SRGroup(2,3,1)", "SRGroup(2,2,1)", [ "SRGroup(2,4,1)", "SRGroup(2,4,2)" ] ],
+#! [ [ (1,5,2,6)(3,7,4,8), (1,3)(2,4)(5,7)(6,8), (1,2)(3,4)(5,6)(7,8) ], "SRGroup(2,3,4)", "SRGroup(2,2,2)", [ "SRGroup(2,4,8)", "SRGroup(2,4,9)", "SRGroup(2,4,10)" ] ], 
+#! [ [ (1,3)(2,4)(5,7)(6,8), (1,5)(2,6)(3,7)(4,8), (1,2)(3,4)(5,6)(7,8) ], "SRGroup(2,3,5)", "SRGroup(2,2,2)", [ "SRGroup(2,4,11)", "SRGroup(2,4,12)", "SRGroup(2,4,13)", "SRGroup(2,4,14)", "SRGroup(2,4,15)" ] ] ]
+#! @EndExampleSession
 DeclareGlobalFunction( "CheckSRProjections" );
 DeclareGlobalFunction( "StringVariables" );
 DeclareGlobalFunction( "UnbindVariables" );
+#! @Description
+#! Returns all of the degrees currently stored in the SRGroups library.
 DeclareGlobalFunction( "SRDegrees" );
+#! @BeginExampleSession
+#! gap> SRDegrees();
+#! [ 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ]
+#! @EndExampleSession
+#! @Description
+#! Returns all of the levels currently stored in the SRGroups library for an input RegularRootedTreeGroupDegree, <A>deg</A>.
 DeclareGlobalFunction( "SRLevels" );
+#! @BeginExampleSession
+#! gap> SRLevels(2);
+#! [ 1, 2, 3, 4 ]
+#! @EndExampleSession
 
 # Package Functions
 #! @Description
